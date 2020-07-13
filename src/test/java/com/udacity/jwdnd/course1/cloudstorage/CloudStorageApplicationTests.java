520961e2f8ab5418adcfa7a73392047793932261
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2020-07-13 23:00:02 +0300

    feat: Add starter project code

---
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CloudStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
