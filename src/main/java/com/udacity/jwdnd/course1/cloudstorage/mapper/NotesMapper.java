51790593948c678ac8df62e1890b71cc5476069e
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2020-07-24 15:20:08 +0300

    feat: Create mapper for notes

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NotesMapper.java
@@ -0,0 +1,25 @@
+package com.udacity.jwdnd.course1.cloudstorage.mapper;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface NotesMapper {
+
+    @Select("SELECT noteid, notetitle, notedescription FROM notes WHERE userid = #{userId}")
+    List<Note> getUserNotes(Integer userId);
+
+    @Insert("INSERT INTO notes (notetitle, notedescription, userid) " +
+            "VALUES (#{noteTitle}, #{noteDescription}, #{userId})")
+    @Options(useGeneratedKeys = true, keyProperty = "noteId")
+    Integer insert(Note note);
+
+    @Update("UPDATE notes SET notetitle = @{noteTitle}, notedescription = #{noteDescription} WHERE noteid = #{noteId}")
+    void update(Note note);
+
+    @Delete("DELETE FROM notes WHERE noteid = @{noteId}")
+    void delete(Integer noteId);
+
+}
