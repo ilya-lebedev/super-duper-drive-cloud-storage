748eda812f4dd725e509b1e136acaa6039acffbd
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2020-07-27 21:23:23 +0300

    feat: Add credentials functionality to the home controller

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,7 +1,11 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.CredentialForm;
 import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.services.CredentialsService;
 import com.udacity.jwdnd.course1.cloudstorage.services.NotesService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,15 +16,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     private NotesService notesService;
+    private CredentialsService credentialsService;
 
-    public HomeController(NotesService notesService) {
+    public HomeController(NotesService notesService, CredentialsService credentialsService) {
         this.notesService = notesService;
+        this.credentialsService = credentialsService;
     }
 
     @GetMapping
     public String view(Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getDetails();
         model.addAttribute("noteForm", new Note());
-        model.addAttribute("notes", notesService.getUserNotes(1));
+        model.addAttribute("notes", notesService.getUserNotes(user.getUserId()));
+        model.addAttribute("credentialForm", new CredentialForm());
+        model.addAttribute("credentials", credentialsService.getUserCredentials(user.getUserId()));
         return "home";
     }
 
