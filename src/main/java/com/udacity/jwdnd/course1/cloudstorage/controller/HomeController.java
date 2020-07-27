9624dde9f69fe6cd7a5cacf6b7777b07733d1857
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2020-07-27 12:08:49 +0300

    feat: Add functionality for notes to the HomeController

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,6 +1,9 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import com.udacity.jwdnd.course1.cloudstorage.services.NotesService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("")
 public class HomeController {
 
+    private NotesService notesService;
+
+    public HomeController(NotesService notesService) {
+        this.notesService = notesService;
+    }
+
     @GetMapping
-    public String view() {
+    public String view(Model model) {
+        model.addAttribute("noteForm", new Note());
+        model.addAttribute("notes", notesService.getUserNotes(1));
         return "home";
     }
 
