6e2fbbb73136d9b1cb9c7e7ab7af3bcea556dc48
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2020-07-29 13:24:24 +0300

    feat: Add files functionality to the home controller

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -4,6 +4,7 @@ import com.udacity.jwdnd.course1.cloudstorage.model.CredentialForm;
 import com.udacity.jwdnd.course1.cloudstorage.model.Note;
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import com.udacity.jwdnd.course1.cloudstorage.service.CredentialsService;
+import com.udacity.jwdnd.course1.cloudstorage.service.FilesService;
 import com.udacity.jwdnd.course1.cloudstorage.service.NotesService;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -15,10 +16,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("")
 public class HomeController {
 
+    private FilesService filesService;
     private NotesService notesService;
     private CredentialsService credentialsService;
 
-    public HomeController(NotesService notesService, CredentialsService credentialsService) {
+    public HomeController(FilesService filesService, NotesService notesService, CredentialsService credentialsService) {
+        this.filesService = filesService;
         this.notesService = notesService;
         this.credentialsService = credentialsService;
     }
@@ -26,10 +29,13 @@ public class HomeController {
     @GetMapping
     public String view(Model model) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getDetails();
+
+        model.addAttribute("files", filesService.getUserFiles(user.getUserId()));
         model.addAttribute("noteForm", new Note());
         model.addAttribute("notes", notesService.getUserNotes(user.getUserId()));
         model.addAttribute("credentialForm", new CredentialForm());
         model.addAttribute("credentials", credentialsService.getUserCredentials(user.getUserId()));
+
         return "home";
     }
 
