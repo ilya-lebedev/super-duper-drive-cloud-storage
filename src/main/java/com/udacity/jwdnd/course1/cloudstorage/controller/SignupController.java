f522c8fd31fed633e0bd15599ff1f4d69faafa34
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2020-07-29 23:21:28 +0300

    feat: Implement redirect from sign up to login page
    Implement redirect from sign up to login page in case of successful sign up

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/signup")
@@ -25,7 +26,8 @@ public class SignupController {
     }
 
     @PostMapping
-    public String signup(@ModelAttribute("signupForm") SignupForm signupForm, Model model) {
+    public String signup(@ModelAttribute("signupForm") SignupForm signupForm, Model model,
+                         RedirectAttributes redirectAttributes) {
 
         String signupError = null;
 
@@ -41,12 +43,13 @@ public class SignupController {
         }
 
         if (signupError == null) {
-            model.addAttribute("signupSuccess", true);
+            redirectAttributes.addFlashAttribute("signupSuccess", true);
+            return "redirect:login";
         } else {
             model.addAttribute("signupError", signupError);
+            return "signup";
         }
 
-        return "signup";
     }
 
 }
